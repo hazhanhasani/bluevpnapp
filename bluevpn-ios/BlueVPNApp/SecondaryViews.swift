d0637f394221fac3c607123d1c250cb58b393129
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-08-27 14:00:15 +0330

    chore(version): prepare BlueVPN 6.0.3

--- bluevpn-ios/BlueVPNApp/SecondaryViews.swift
+++ bluevpn-ios/BlueVPNApp/SecondaryViews.swift
@@ -148,7 +148,7 @@ struct SettingsView: View {
                 Button("مکان‌ها") { store.path.append(.locations) }
                 Button("پلن‌ها") { store.path.append(.plans) }
                 Button("پشتیبانی") { store.path.append(.support) }
-                LabeledContent("نسخه", value: "6.0.2")
+                LabeledContent("نسخه", value: "6.0.3")
             }
         }
         .navigationTitle("تنظیمات")

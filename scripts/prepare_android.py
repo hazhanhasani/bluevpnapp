62ed854f28c306d74261cd3334fe18579a542bcc
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-08-28 13:27:02 +0330

    build(android): overlay latency state model

--- scripts/prepare_android.py
+++ scripts/prepare_android.py
@@ -868,6 +868,7 @@ def inject_bluevpn_home() -> None:
         bluevpn_dir / "BlueVpnBootstrap.kt": ROOT / "android-source/BlueVpnBootstrap.kt",
         bluevpn_dir / "BlueVpnRuntimeGate.kt": ROOT / "android-source/BlueVpnRuntimeGate.kt",
         bluevpn_dir / "BlueVpnRefreshCoordinator.kt": ROOT / "android-source/BlueVpnRefreshCoordinator.kt",
+        bluevpn_dir / "BlueVpnLatencyState.kt": ROOT / "android-source/BlueVpnLatencyState.kt",
         bluevpn_dir / "BlueVpnEntitlement.kt": ROOT / "android-source/BlueVpnEntitlement.kt",
         bluevpn_dir / "BlueVpnSmartSelector.kt": ROOT / "android-source/BlueVpnSmartSelector.kt",
         bluevpn_dir / "BlueVpnTapsellManager.kt": ROOT / "android-source/BlueVpnTapsellManager.kt",

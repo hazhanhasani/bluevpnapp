a1915d5d390b644eb5c0e9697b16cf67001d4c2c
hazhanhasani <hazhanhasani4268@atomicmail.io>
2026-08-31 02:31:11 +0330

    release: sync iOS and Windows clients to 6.1.4

--- bluevpn-ios/BlueVPNApp/SecondaryViews.swift
+++ bluevpn-ios/BlueVPNApp/SecondaryViews.swift
@@ -148,7 +148,7 @@ struct SettingsView: View {
                 Button("مکان‌ها") { store.path.append(.locations) }
                 Button("پلن‌ها") { store.path.append(.plans) }
                 Button("پشتیبانی") { store.path.append(.support) }
-                LabeledContent("نسخه", value: "6.1.3")
+                LabeledContent("نسخه", value: "6.1.4")
             }
         }
         .navigationTitle("تنظیمات")
